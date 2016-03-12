6dfe9bd532df9a57bd9b65f24ca5d873518b9f79
Lina Lumburovska <linalumburovska20@gmail.com>
2016-03-12 13:08:59 +0000

    Dodajanje opomnikov

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -2,13 +2,29 @@ window.addEventListener('load', function() {
 	//stran nalozena
 	
 	var potrdiPrijavo = function() {
+		
 		var uporabnik = document.querySelector("#uporabnisko_ime").value;
 		document.querySelector("#uporabnik").innerHTML = uporabnik;
 		document.querySelector(".pokrivalo").style.visibility = "hidden";
-		}
+		
+	}
+	document.querySelector("#prijavniGumb").addEventListener('click', potrdiPrijavo);
 	
-	document.querySelector("#prijavniGumb").addEventListener("click", potrdiPrijavo);
+	var dodajOpomnik = function() {
+		
+		var naziv_opomnika = document.querySelector("#naziv_opomnika").value;
+		var cas_opomnika = document.querySelector("#cas_opomnika").value;
+		document.querySelector("#naziv_opomnika").value = "";
+		document.querySelector("#cas_opomnika").value = "";
+		
+		document.querySelector("#opomniki").innerHTML += " \
+		<div class='opomnik senca rob'> \
+            <div class='naziv_opomnika'>" + naziv_opomnika + "</div> \
+            <div class='cas_opomnika'> Opomnik čez <span>" + cas_opomnika + "</span> sekund.</div> \
+		</div>"
+	}
 	
+	document.querySelector("#dodajGumb").addEventListener('click', dodajOpomnik);
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
@@ -21,6 +37,8 @@ window.addEventListener('load', function() {
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
+			
+			
 		}
 	}
 	setInterval(posodobiOpomnike, 1000);
